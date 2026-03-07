93db3bbb18ae53577e29abf3cd4186ebe46d28ab
tk2217 <git@tk2217.com>
2026-03-07 12:59:43 -0600

    gearing fix

--- src/main/java/edu/msoe/cybercheese/trinity/subsystems/intake/IntakeConstants.java
+++ src/main/java/edu/msoe/cybercheese/trinity/subsystems/intake/IntakeConstants.java
@@ -8,7 +8,7 @@ public class IntakeConstants {
     public static final int LOWER_MOTOR_ID = 32;
     public static final MotorConfig LOWER_MOTOR_CONFIG = MotorConfig.vortexBuilder(
                     MotorConfig.ControllerKind.MAX, MotorConfig.ControlMode.POSITION)
-            .gearing(27. * (50. / 18.))
+            .gearing(27. * (50. / 22.))
             .encoderGearing(1.)
             .pid(new PIDConstants(2, 0, 0))
             .build();
