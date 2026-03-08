7b5c8fef9be6367ae21198da074365929f72b145
tk2217 <git@tk2217.com>
2026-03-08 10:44:40 -0500

    .

--- src/main/java/edu/msoe/cybercheese/trinity/RobotContainer.java
+++ src/main/java/edu/msoe/cybercheese/trinity/RobotContainer.java
@@ -98,13 +98,12 @@ public class RobotContainer {
         this.autoFactory = new AutoFactory(
                 this.drive::getPose, this.drive::setPose, this.drive::followTrajectory, true, this.drive);
 
-        this.autoFactory.bind("shoot", Commands.parallel(
-                        ShooterCommands.runVelocity(
-                                this.shooter,
-                                430), // this works, don't touch!
+        this.autoFactory.bind(
+                "shoot",
+                Commands.parallel(
+                        ShooterCommands.runVelocity(this.shooter, 430), // this works, don't touch!
                         LoaderCommands.shootWhenReady(this.loader, this.shooter, 80),
-                HopperCommands.runVelocity(this.hopper, () -> -2700)
-        ));
+                        HopperCommands.runVelocity(this.hopper, () -> -2700)));
 
         for (final var trajName : Choreo.availableTrajectories()) {
             System.out.println("Loading Trajectory: " + trajName);
