053ca1d8b7a2152b40f208499a27c80d6eb22de6
tk2217 <git@tk2217.com>
2026-03-23 18:04:47 -0500

    should fix some aiming stuff

--- src/main/java/edu/msoe/cybercheese/trinity/subsystems/shooter/ShooterMath.java
+++ src/main/java/edu/msoe/cybercheese/trinity/subsystems/shooter/ShooterMath.java
@@ -8,6 +8,11 @@ import edu.wpi.first.math.geometry.Translation2d;
 public class ShooterMath {
 
     private static final double HUB_HEIGHT = 1.8161;
+    private static final double RELEASE_HEIGHT = 0.440;
+    private static final double HUB_HEIGHT_OFFSET = HUB_HEIGHT - RELEASE_HEIGHT;
+
+    private static final double SHOOT_OFFSET = -0.211;
+
     private static final Translation2d HUB_POS_BLUE = new Translation2d(4.6256194, 4.0346376);
     private static final Translation2d HUB_POS_RED = ChoreoAllianceFlipUtil.flip(HUB_POS_BLUE);
     private static final double HUB_RADIUS = 1.052373 / 2.;
@@ -22,7 +27,9 @@ public class ShooterMath {
     }
 
     public static Translation2d relativeHubPos(final Pose2d robotPose) {
-        return hubPos().minus(robotPose.getTranslation())
+        final var shooterTranslation = robotPose.getTranslation().plus(new Translation2d(SHOOT_OFFSET, 0.));
+
+        return hubPos().minus(shooterTranslation)
                 .rotateBy(robotPose.getRotation().unaryMinus());
     }
 
@@ -33,6 +40,6 @@ public class ShooterMath {
         final var minDistance = relativeHubPos.getX() - difference;
         final var maxDistance = relativeHubPos.getX() + difference;
 
-        return ExternalShooterMath.calculateTrajectory(minDistance, maxDistance, HUB_HEIGHT);
+        return ExternalShooterMath.calculateTrajectory(minDistance, maxDistance, HUB_HEIGHT_OFFSET);
     }
 }
