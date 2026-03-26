b0a458090049f0c71bd3c0a768335bece1602cd9
tk2217 <git@tk2217.com>
2026-03-26 18:43:40 -0500

    lights

---
+++ whitesands/main.py
@@ -0,0 +1,163 @@
+import neopixel
+import itertools
+from neopixel import NeoPixel
+from typing import Optional
+from time import sleep
+import argparse
+from os.path import basename
+import ntcore
+from time import time
+import board
+
+C_NOCONN = 0
+# orange
+C_ESTOP = 0xF4A524
+
+C_RED = 0xFF0000
+C_BLUE = 0x0000FF
+
+# teal
+C_FMS = 0x77EABC
+# purple
+C_PHOTON = 0x7839DD
+# yellow
+C_CAN = 0xC1ED3D
+
+
+AKIT_CONN_KEYS = [
+    "Drive/Gyro",
+    "Hopper",
+    "Intake",
+    "IntakeRoller",
+    "Loader",
+]
+
+for i in range(4):
+    for c in ["drive", "turn"]:
+        AKIT_CONN_KEYS.append(f"Drive/{i}/{c}")
+
+
+class CCNeoPixel:
+    inner: NeoPixel
+
+    def __init__(self, pin, count: int):
+        self.inner = NeoPixel(pin, count, auto_write=False, pixel_order=neopixel.RGB)
+
+    def set_all(self, color: int):
+        self.inner.fill(color)
+        self.inner.show()
+
+    def set_cycle(self, colors: list[int]):
+        it = itertools.cycle(colors)
+
+        for i in range(self.inner.n):
+            self.inner[i] = next(it)
+
+        self.inner.show()
+
+
+class HeartbeatSubscriber:
+    last: int
+    time: float
+
+    def __init__(self):
+        self.last = -1
+        self.time = -1
+
+    def update(self, value: Optional[int]) -> bool:
+        new_time = time()
+
+        if value is None:
+            self.last = -1
+            self.time = -1
+            return False
+
+        if self.last != value:
+            self.last = value
+            self.time = new_time
+
+            return True
+
+        return new_time > self.time + 0.5
+
+
+def run_nt(nt: ntcore.NetworkTableInstance, neopixel: CCNeoPixel):
+    cameras = {
+        "FrontThrifty": HeartbeatSubscriber(),
+        "BackThrifty": HeartbeatSubscriber(),
+    }
+
+    fms_info = nt.getTable("FMSInfo")
+    pv = nt.getTable("photonvision")
+    akit = nt.getTable("AdvantageKit")
+    ds = nt.getTable("AdvantageKit/DriverStation")
+
+    while True:
+        sleep(0.1)
+
+        if not nt.isConnected():
+            neopixel.set_all(C_NOCONN)
+            continue
+
+        estop = ds.getBoolean("EmergencyStop", False)
+
+        if estop:
+            neopixel.set_all(C_ESTOP)
+            continue
+
+        is_red = fms_info.getBoolean("IsRedAlliance", False)
+        fms_connected = pv.getBoolean("FMSAttached", False)
+
+        pv_connected = True
+        for name, hb in cameras.items():
+            connected = hb.update(pv.getNumber(name + "/heartbeat", None))
+            pv_connected &= connected
+
+        can_connected = True
+        for k in AKIT_CONN_KEYS:
+            connected = akit.getBoolean(k + "/connected", False)
+            can_connected &= connected
+
+        team_color = C_RED if is_red else C_BLUE
+
+        colors = [team_color]
+        if not fms_connected:
+            colors.append(C_FMS)
+        if not pv_connected:
+            colors.append(C_PHOTON)
+        if not can_connected:
+            colors.append(C_CAN)
+
+        neopixel.set_cycle(colors)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p",
+        "--protocol",
+        type=int,
+        choices=[3, 4],
+        help="NT Protocol to use",
+        default=4,
+    )
+    parser.add_argument("ip", type=str, help="IP address to connect to")
+    args = parser.parse_args()
+
+    inst = ntcore.NetworkTableInstance.getDefault()
+
+    identity = basename(__file__)
+    if args.protocol == 3:
+        inst.startClient3(identity)
+    else:
+        inst.startClient4(identity)
+
+    inst.setServer(args.ip)
+
+    np = CCNeoPixel(board.D18, 64)
+
+    run_nt(inst, np)
+
+
+if __name__ == "__main__":
+    main()
