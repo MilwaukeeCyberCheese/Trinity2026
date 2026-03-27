38848fb870c280d29c0e5d5a2322ffb391b1ef38
tk2217 <git@tk2217.com>
2026-03-26 19:14:39 -0500

    what

--- whitesands/build.sh
+++ whitesands/build.sh
@@ -8,9 +8,9 @@ cp setup.sh builddir
 cp main.py builddir
 pip download \
 	--only-binary=:all: \
-	--platform manylinux2_35_aarch64 \
-	--python-version 3.11 \
+	--platform manylinux2_34_aarch64 \
+	--python-version 3.13 \
 	--implementation cp \
-	--abi cp311 \
+	--abi cp313 \
 	-r builddir/requirements.txt \
 	-d builddir/wheeldir
